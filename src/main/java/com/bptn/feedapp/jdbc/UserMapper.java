0e9c5d24b837d16b4d64508dc505f877a3624ddb
getenesh <getenesh.guta>
2024-11-04 19:40:22 -0500

    formated source code

--- src/main/java/com/bptn/feedapp/jdbc/UserMapper.java
+++ src/main/java/com/bptn/feedapp/jdbc/UserMapper.java
@@ -10,22 +10,22 @@ class UserMapper implements RowMapper<UserBean> {
 	@Override
 	public UserBean mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-/* Create a UserBean object*/
-UserBean user = new UserBean();
+		/* Create a UserBean object */
+		UserBean user = new UserBean();
 
-/* Populates the UserBean object with data from the resultSet */
-user.setUserId(rs.getInt("userId"));
-user.setFirstName(rs.getString("firstName"));
-user.setLastName(rs.getString("lastName"));
-user.setUsername(rs.getString("username"));
-user.setPassword(rs.getString("password"));
-user.setPhone(rs.getString("phone"));
-user.setEmailId(rs.getString("emailId"));
-user.setEmailVerified(rs.getBoolean("emailVerified"));
-user.setCreatedOn(rs.getTimestamp("createdOn"));
+		/* Populates the UserBean object with data from the resultSet */
+		user.setUserId(rs.getInt("userId"));
+		user.setFirstName(rs.getString("firstName"));
+		user.setLastName(rs.getString("lastName"));
+		user.setUsername(rs.getString("username"));
+		user.setPassword(rs.getString("password"));
+		user.setPhone(rs.getString("phone"));
+		user.setEmailId(rs.getString("emailId"));
+		user.setEmailVerified(rs.getBoolean("emailVerified"));
+		user.setCreatedOn(rs.getTimestamp("createdOn"));
 
-/* Return the populated UserBean object */
-return user;
+		/* Return the populated UserBean object */
+		return user;
 	}
 
 }
