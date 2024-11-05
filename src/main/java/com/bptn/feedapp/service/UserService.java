f520d77028de507b376125cc96f4846665245949
getenesh <getenesh.guta>
2024-11-05 14:18:43 -0500

    UserService modification

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -1,28 +1,29 @@
 package com.bptn.feedapp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bptn.feedapp.jdbc.UserBean;
-import com.bptn.feedapp.jdbc.UserDao;
+import com.bptn.feedapp.jpa.User;
+import com.bptn.feedapp.repository.UserRepository;
 
 @Service
 public class UserService {
 	@Autowired
-	UserDao userDao;
+	UserRepository userRepository;
 
-	public List<UserBean> listUsers() {
-		return this.userDao.listUsers();
+	public List<User> listUsers() {
+		return this.userRepository.findAll();
 	}
 
-	public UserBean findByUsername(String username) {
-		return this.userDao.findByUsername(username);
-	}
+	public Optional<User> findByUsername(String username)  {
+		return this.userRepository.findByUsername(username);	
+		}
 
-	public void createUser(UserBean user) {
-		this.userDao.createUser(user);
+	public void createUser(User user) {
+		this.userRepository.save(user);
 	}
 
 }
