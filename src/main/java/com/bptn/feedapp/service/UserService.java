369da220829949789081c75cc0be93b46899c229
getenesh <getenesh.guta>
2024-11-04 19:34:56 -0500

    create service package and feedApp userService

---
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -0,0 +1,24 @@
+package com.bptn.feedapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedapp.jdbc.UserBean;
+import com.bptn.feedapp.jdbc.UserDao;
+
+@Service
+public class UserService {
+	@Autowired
+	UserDao userDao;
+
+	public List<UserBean> listUsers() {
+		return this.userDao.listUsers();
+	}
+
+	public void createUser(UserBean user) {
+		this.userDao.createUser(user);
+	}
+
+}
