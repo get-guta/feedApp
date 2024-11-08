fbbd772649ac2eda461890f17695cbb5a74d0318
getenesh <getenesh.guta>
2024-11-08 11:21:23 -0500

    latest and formated

--- src/main/java/com/bptn/feedapp/service/EmailService.java
+++ src/main/java/com/bptn/feedapp/service/EmailService.java
@@ -19,62 +19,62 @@ import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
-
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Value("${spring.mail.username}")
 	private String emailFrom;
-	
+
 	@Autowired
 	JwtService jwtService;
-	    
+
 	@Autowired
 	ResourceProvider provider;
 
 	@Autowired
 	TemplateEngine templateEngine;
-	    
+
 	@Autowired
 	JavaMailSender javaMailSender;
-	
-	private void sendEmail(User user, String clientParam, String templateName, String emailSubject, long expiration) {
-	     
-	     try {
-	         
-	         /* Collect Data for the Email HTML generation */
-	         Context context = new Context();
-	         context.setVariable("user", user);
-	         context.setVariable("client", this.provider.getClientUrl());            
-	         context.setVariable("param", clientParam);
-	         context.setVariable("token", this.jwtService.generateJwtToken(user.getUsername(),expiration));
-	         
-	         /* Process Email HTML Template */
-	         String process = this.templateEngine.process(templateName, context);
-	         
-	         MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
-	         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-	         
-	         /* Set Email Information */
-	         helper.setFrom(this.emailFrom, "FeedApp - Obsidi Academy");
-	         helper.setSubject(emailSubject);
-	         helper.setText(process, true);
-	         helper.setTo(user.getEmailId());
-	         
-	         /* Send Email */
-	         this.javaMailSender.send(mimeMessage);
-	         
-	         this.logger.debug("Email Sent, {} ", user.getEmailId());
 
-	     } catch (Exception ex) {
-	         
-	         this.logger.error("Error while Sending Email, Username: " + user.getUsername(), ex);
-	     }
+	private void sendEmail(User user, String clientParam, String templateName, String emailSubject, long expiration) {
+
+		try {
+
+			/* Collect Data for the Email HTML generation */
+			Context context = new Context();
+			context.setVariable("user", user);
+			context.setVariable("client", this.provider.getClientUrl());
+			context.setVariable("param", clientParam);
+			context.setVariable("token", this.jwtService.generateJwtToken(user.getUsername(), expiration));
+
+			/* Process Email HTML Template */
+			String process = this.templateEngine.process(templateName, context);
+
+			MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+			/* Set Email Information */
+			helper.setFrom(this.emailFrom, "FeedApp - Obsidi Academy");
+			helper.setSubject(emailSubject);
+			helper.setText(process, true);
+			helper.setTo(user.getEmailId());
+
+			/* Send Email */
+			this.javaMailSender.send(mimeMessage);
+
+			this.logger.debug("Email Sent, {} ", user.getEmailId());
+
+		} catch (Exception ex) {
+
+			this.logger.error("Error while Sending Email, Username: " + user.getUsername(), ex);
+		}
 	}
+
 	@Async
 	public void sendVerificationEmail(User user) {
-	     
-	 this.sendEmail(user, this.provider.getClientVerifyParam(),"verify_email", 
-	 String.format("Welcome %s %s",user.getFirstName(),user.getLastName()), 
-	                   this.provider.getClientVerifyExpiration());
-	}    
+
+		this.sendEmail(user, this.provider.getClientVerifyParam(), "verify_email",
+				String.format("Welcome %s %s", user.getFirstName(), user.getLastName()),
+				this.provider.getClientVerifyExpiration());
+	}
 }
