b91bcb50dd7782a245bc1fd1be2788ae6cbcc044
getenesh <getenesh.guta>
2024-11-11 09:02:55 -0500

    Testing configuration

---
+++ src/main/java/com/bptn/feedapp/config/WebTestConfig.java
@@ -0,0 +1,26 @@
+package com.bptn.feedapp.config;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bptn.feedapp.provider.ResourceProvider;
+
+@Configuration
+@Profile("test")
+public class WebTestConfig {
+
+	@Autowired
+	ResourceProvider resourceProvider;
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	Server inMemoryH2DatabaseServer() throws SQLException {
+
+		return Server.createTcpServer(this.resourceProvider.getH2ServerParams());
+
+	}
+}
