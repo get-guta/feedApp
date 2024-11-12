64b27a6b3a6383a108194ba897dfc56566b1ed81
getenesh <getenesh.guta>
2024-11-12 10:34:51 -0500

    password reset userController

--- src/main/java/com/bptn/feedapp/controller/UserController.java
+++ src/main/java/com/bptn/feedapp/controller/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @CrossOrigin(exposedHeaders = "Authorization")
 @RestController
@@ -106,6 +107,14 @@ public class UserController {
 		logger.debug("Sending Reset Password Email, emailId: {}", emailId);
 
 		this.userService.sendResetPasswordEmail(emailId);
+	}
+
+	@PostMapping("/reset")
+	public void passwordReset(@RequestBody JsonNode json) {
+
+		logger.debug("Resetting Password, password: {}", json.get("password").asText());
+
+		this.userService.resetPassword(json.get("password").asText());
 	}
 
 }
