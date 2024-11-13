1e430236c6dc353207cd096b48a131bbd6495323
getenesh <getenesh.guta>
2024-11-13 11:42:09 -0500

    Create FeedMetaData - FeedController

--- src/main/java/com/bptn/feedapp/controller/FeedController.java
+++ src/main/java/com/bptn/feedapp/controller/FeedController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bptn.feedapp.domain.PageResponse;
 import com.bptn.feedapp.jpa.Feed;
+import com.bptn.feedapp.jpa.FeedMetaData;
 import com.bptn.feedapp.service.FeedService;
 
 @CrossOrigin
@@ -55,6 +56,14 @@ public class FeedController {
 		logger.debug("Getting Other Users Feeds List, pageNum: {}, pageSize: {}", pageNum, pageSize);
 
 		return this.feedService.getOtherUsersFeeds(pageNum, pageSize);
+	}
+
+	@PostMapping("/meta/{feedId}")
+	public FeedMetaData createFeedMetaData(@PathVariable int feedId, @RequestBody FeedMetaData meta) {
+
+		logger.debug("Creating FeedMetaData, feedId: {}", feedId);
+
+		return this.feedService.createFeedMetaData(feedId, meta);
 	}
 
 }
